e9eb6210e484b26dfde900feebca9443298ea37a
Scaxlibur <scaxlibur@gmail.com>
2025-03-13 16:43:54 +0800

    AD9834测试

---
+++ main/main.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "rotaryEncoder.hpp"
+#include "freertos/FreeRTOS.h"
+#include "AD9834.hpp"
+
+extern "C" void app_main(void)
+{
+
+    AD9834_Init();
+    uint32_t freq = 1000;
+    while (true)
+    {
+        AD9834_Select_Wave(Sine_Wave);
+        freq = 10000;
+        while (freq <= 50000)
+        {
+            AD9834_Set_Freq(FREQ_0, freq);
+            freq += 10000;
+            vTaskDelay(500/ portTICK_PERIOD_MS);
+        }
+        AD9834_Select_Wave(Triangle_Wave);
+        freq = 10000;
+        while (freq <= 50000)
+        {
+            AD9834_Set_Freq(FREQ_0, freq);
+            freq += 10000;
+            vTaskDelay(500/ portTICK_PERIOD_MS);
+        }
+        AD9834_Select_Wave(Square_Wave);
+        freq = 10000;
+        while (freq <= 50000)
+        {
+            AD9834_Set_Freq(FREQ_0, freq);
+            freq += 10000;
+            vTaskDelay(500/ portTICK_PERIOD_MS);
+        }
+        vTaskDelay(50 / portTICK_PERIOD_MS);
+    }
+}
