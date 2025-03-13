84fb8584a9f428a98a83f498c59d4471da71fc7f
Scaxlibur <scaxlibur@gmail.com>
2025-03-13 16:43:41 +0800

    AD9834驱动调试完成

--- main/AD9834.cpp
+++ main/AD9834.cpp
@@ -1,29 +1,20 @@
 #include "AD9834.hpp"
 
-/*
-函数名：AD9833_Write_Data
-
-功  能：向AD9834写入16位数据
-
-参  数：data -- 16位数据
-
-返回值：无
-*/
-
 void AD9834_Write_Data(unsigned int data)
 {
-	unsigned char i =0;
-	
+
+	unsigned char i = 0;
+
 	AD9834_SCLK_SET;
 	AD9834_FSYNC_CLR;
-	
-	for(i= 0;i < 16;	i++)
+
+	for (i = 0; i < 16; i++)
 	{
-		if(data & 0x8000)
+		if (data & 0x8000)
 			AD9834_SDATA_SET;
 		else
 			AD9834_SDATA_CLR;
-		
+
 		AD9834_SCLK_CLR;
 		data <<= 1;
 		AD9834_SCLK_SET;
@@ -31,116 +22,63 @@ void AD9834_Write_Data(unsigned int data)
 	AD9834_SDATA_SET;
 	AD9834_FSYNC_SET;
 }
-/*
-函数名：AD9834_Init
-
-功  能：AD9834初始化
-
-参  数：无
-
-返回值：无
-*/
 
 void AD9834_Init(void)
 {
-    /*
-	GPIO_InitTypeDef GPIO_InitStruct;
-	
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA|RCC_APB2Periph_GPIOE|RCC_APB2Periph_GPIOC,ENABLE);
-	
-	GPIO_InitStruct.GPIO_Pin = AD9834_FSYNC|AD9834_SDATA|AD9834_RESET;
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
-	
-	GPIO_Init(GPIOA,&GPIO_InitStruct);
-	
-	GPIO_InitStruct.GPIO_Pin = AD9834_SCLK;
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
-	
-	GPIO_Init(GPIOC,&GPIO_InitStruct);
-	
-	GPIO_InitStruct.GPIO_Pin = AD9834_FS|AD9834_PS;
-	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
-	
-	GPIO_Init(GPIOC,&GPIO_InitStruct);
-	*/
-    gpio_config(&AD9834_controlPins_conf);
-	
-    //GPIO_ResetBits(GPIOC,AD9834_FS);
-    gpio_set_level(AD9834_FS, 0);
+	gpio_config_t AD9834_controlPins_conf = {
+		.pin_bit_mask = (1ULL << AD9834_FSYNC) |
+						(1ULL << AD9834_SCLK) |
+						(1ULL << AD9834_SDATA) |
+						(1ULL << AD9834_RESET) |
+						(1ULL << AD9834_FS) |
+						(1ULL << AD9834_PS),
+		.mode = GPIO_MODE_OUTPUT,
+		.pull_up_en = GPIO_PULLUP_ENABLE,
+		.pull_down_en = GPIO_PULLDOWN_ENABLE,
+		.intr_type = GPIO_INTR_DISABLE,
+	};
+	gpio_config(&AD9834_controlPins_conf);
+	gpio_set_level(AD9834_FS, 0);
+	gpio_set_level(AD9834_PS, 0);
 
-	//GPIO_ResetBits(GPIOC,AD9834_PS);
-    gpio_set_level(AD9834_PS, 0);
-	
 	AD9834_Write_Data(0x2100);
 	AD9834_Write_Data(0x2038);
 	AD9834_Write_Data(0XC000);
 	AD9834_Write_Data(0x2100);
 }
-/*
-函数名：AD9834_Select_Wave
 
-功  能：波形选择		？？为什么要进行拉高RESET复位
+void AD9834_Select_Wave(waveType_t waveType)
 
-参  数：initdata -- 波形数据
-
-返回值：无
-*/
-
-void AD9834_Select_Wave(unsigned int initdata) 
-
-{ 
-
-    AD9834_FSYNC_SET; 
-
-    AD9834_SCLK_SET; 
-
-     
-
-    AD9834_RESET_SET; 
-
-    AD9834_RESET_SET; 
-
-    AD9834_RESET_CLR;
-
-
-    AD9834_Write_Data(initdata); 
-
-} 
-
-/*
-函数名：AD9834_Set_Freq
-
-功  能：频率写入
-
-参  数：freq_number -- 频率寄存器选择
-				freq -- 频率值
-
-返回值：无
-*/
-
-void AD9834_Set_Freq(unsigned char freq_number, unsigned long freq) 
 {
-    unsigned long FREQREG = (unsigned long)(268435456.0/AD9834_MCLK*freq); 
-    unsigned int FREQREG_LSB_14BIT = (unsigned int)FREQREG; 
-    unsigned int FREQREG_MSB_14BIT = (unsigned int)(FREQREG>>14); 
-	
-    if(freq_number == FREQ_0) 
-    { 
-        FREQREG_LSB_14BIT &= ~(1U<<15); 
-        FREQREG_LSB_14BIT |= 1<<14; 
-        FREQREG_MSB_14BIT &= ~(1U<<15); 
-        FREQREG_MSB_14BIT |= 1<<14; 
-    }
-    else
-    {
-        FREQREG_LSB_14BIT &= ~(1<<14); 
-        FREQREG_LSB_14BIT |= 1U<<15; 
-        FREQREG_MSB_14BIT &= ~(1<<14); 
-        FREQREG_MSB_14BIT |= 1U<<15; 
-    } 
-    AD9834_Write_Data(FREQREG_LSB_14BIT); 
-    AD9834_Write_Data(FREQREG_MSB_14BIT);      
-} 
+	AD9834_FSYNC_SET;
+	AD9834_SCLK_SET;
+	AD9834_RESET_SET;
+	AD9834_RESET_SET;
+	AD9834_RESET_CLR;
+	// unsigned int initdata = waveType;
+	AD9834_Write_Data(waveType);
+}
+
+void AD9834_Set_Freq(freq_register_t freq_register, uint32_t freq)
+{
+	unsigned long FREQREG = (unsigned long)(268435456.0 / AD9834_MCLK * freq);
+	unsigned int FREQREG_LSB_14BIT = (unsigned int)FREQREG;
+	unsigned int FREQREG_MSB_14BIT = (unsigned int)(FREQREG >> 14);
+
+	if (freq_register == FREQ_0)
+	{
+		FREQREG_LSB_14BIT &= ~(1U << 15);
+		FREQREG_LSB_14BIT |= 1 << 14;
+		FREQREG_MSB_14BIT &= ~(1U << 15);
+		FREQREG_MSB_14BIT |= 1 << 14;
+	}
+	else
+	{
+		FREQREG_LSB_14BIT &= ~(1 << 14);
+		FREQREG_LSB_14BIT |= 1U << 15;
+		FREQREG_MSB_14BIT &= ~(1 << 14);
+		FREQREG_MSB_14BIT |= 1U << 15;
+	}
+	AD9834_Write_Data(FREQREG_LSB_14BIT);
+	AD9834_Write_Data(FREQREG_MSB_14BIT);
+}
--- main/AD9834.hpp
+++ main/AD9834.hpp
@@ -4,32 +4,26 @@
 #include "driver/spi_master.h"
 #include "driver/gpio.h"
 
+typedef enum{
+    Sine_Wave = 0x2008,     //正弦波
+    Triangle_Wave = 0x2002,    //三角波
+    Square_Wave = 0x2028,    //方波
+}waveType_t;
 
-
-#define Sine_Wave     0x2008	//正弦波
-#define Triangle_Wave 0x2002	//三角波
-#define Square_Wave   0x2028	//方波
+typedef enum{
+    FREQ_0 = 0,
+    FREQ_1 = 1,
+}freq_register_t;
 
 #define AD9834_MCLK 75000000UL	//AD9834晶振频率
 
-//控制脚A3-A6，FS，PS即B0,B1直接置低，由控制位控制
-#define AD9834_FSYNC  			    GPIO_NUM_11
-#define AD9834_SCLK   			    GPIO_NUM_12
-#define AD9834_SDATA  			    GPIO_NUM_13
-#define AD9834_RESET				GPIO_NUM_14
-#define AD9834_FS 					GPIO_NUM_15
-#define AD9834_PS 					GPIO_NUM_16
-
-/*
-#define AD9834_FSYNC_SET	GPIO_SetBits(AD9834_Control_Port,AD9834_FSYNC)
-#define AD9834_FSYNC_CLR   	GPIO_ResetBits(AD9834_Control_Port ,AD9834_FSYNC) 
-#define AD9834_SCLK_SET		GPIO_SetBits(GPIOC ,AD9834_SCLK) 
-#define AD9834_SCLK_CLR		GPIO_ResetBits(GPIOC ,AD9834_SCLK) 
-#define AD9834_SDATA_SET   	GPIO_SetBits(AD9834_Control_Port ,AD9834_SDATA) 
-#define AD9834_SDATA_CLR   	GPIO_ResetBits(AD9834_Control_Port ,AD9834_SDATA) 
-#define AD9834_RESET_SET   	GPIO_SetBits(AD9834_Control_Port ,AD9834_RESET) 
-#define AD9834_RESET_CLR   	GPIO_ResetBits(AD9834_Control_Port ,AD9834_RESET)
-*/
+//连接引脚
+#define AD9834_FSYNC  			    GPIO_NUM_4
+#define AD9834_SCLK   			    GPIO_NUM_6
+#define AD9834_SDATA  			    GPIO_NUM_5
+#define AD9834_RESET				GPIO_NUM_20
+#define AD9834_FS 					GPIO_NUM_7
+#define AD9834_PS 					GPIO_NUM_8
 
 #define AD9834_FSYNC_SET	gpio_set_level(AD9834_FSYNC, 1)
 #define AD9834_FSYNC_CLR   	gpio_set_level(AD9834_FSYNC, 0) 
@@ -40,31 +34,28 @@
 #define AD9834_RESET_SET   	gpio_set_level(AD9834_RESET, 1) 
 #define AD9834_RESET_CLR   	gpio_set_level(AD9834_RESET, 0)
 
-//频率寄存器
-#define FREQ_0      0 
-#define FREQ_1      1 
+/** 
+ * @brief 向AD9834写入16位数据
+ * @param data 写入的数据
+*/
+void AD9834_Write_Data(unsigned int data);
 
-gpio_config_t AD9834_controlPins_conf ={
-    .pin_bit_mask = (1ULL<<AD9834_FSYNC) | 
-                    (1ULL<<AD9834_SCLK) |
-                    (1ULL<<AD9834_SDATA) |
-                    (1ULL<<AD9834_RESET) |
-                    (1ULL<<AD9834_FS) |
-                    (1ULL<<AD9834_PS),
-    .mode = GPIO_MODE_OUTPUT,
-    .pull_up_en = GPIO_PULLUP_ENABLE,
-    .pull_down_en = GPIO_PULLDOWN_ENABLE,
-    .intr_type = GPIO_INTR_DISABLE,
-};
+/**
+ * @brief 波形选择
+ * @param waveType_t 波形种类
+ */
+void AD9834_Select_Wave(waveType_t waveType) ;
 
-/* AD9834函数声明 */ 
+/**
+ * @brief 初始化配置
+ */
+void AD9834_Init(void);
 
-void AD9834_Write_Data(unsigned int data);			                    //写16位数据
-
-void AD9834_Select_Wave(unsigned int initdata) ;                        //选择输出波形 
-
-void AD9834_Init(void);  										        //初始化配置 
-
-void AD9834_Set_Freq(unsigned char freq_number, unsigned long freq) ;	//选择输出寄存器和输出频率值 
+/**
+ * @brief 频率写入
+ * @param freq_register 频率寄存器选择
+ * @param freq 频率，单位Hz
+ */
+void AD9834_Set_Freq(freq_register_t freq_register, uint32_t freq);
 	
 #endif
